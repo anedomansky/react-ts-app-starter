6235ae9387760c70344a22cd9be4e46041a14636
anedomansky <anedomansky@googlemail.com>
2021-11-25 19:26:00 +0100

    Removed unnecessary code from index.ts template file.

--- src/templates/src/index.ts
+++ src/templates/src/index.ts
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/app/App';
 
-// needed in order to enable HMR
-declare let module: __WebpackModuleApi.Module;
-
 ReactDOM.render(
     <React.StrictMode><App /></React.StrictMode>,
     document.getElementById('app'),
 );
 
-// enable HMR for the client side app
-if (module.hot) {
-    module.hot.accept();
-}
 `;
 
 export default content;
